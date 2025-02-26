24fa5da501377ffc5a6eae22d2069f198ef9b264
Marabii <minehamza97@gmail.com>
2025-02-26 17:53:20 +0100

    fix prod error

--- src/components/HomePage/OrdersMap/components/mapForTravelers.tsx
+++ src/components/HomePage/OrdersMap/components/mapForTravelers.tsx
@@ -267,14 +267,6 @@ function OrderList({
                     { code: exchangeRate.target }
                   )}
                 </h2>
-
-                <Link
-                  href={`/negotiate?recipientId=${order.buyerId}&orderId=${order._id}`}
-                  className="bg-white text-purple-600 font-bold px-4 py-2 rounded-lg 
-                         shadow-md hover:bg-opacity-90 transition-colors"
-                >
-                  View Details
-                </Link>
               </motion.div>
             </motion.li>
           );
